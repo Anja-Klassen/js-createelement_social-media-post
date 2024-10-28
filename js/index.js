f863f40f41e24608b9e59726ee01f4788c914a64
Anja-Klassen <anja.klassen@posteo.de>
2024-10-28 12:32:56 +0100

    add solution

--- js/index.js
+++ js/index.js
@@ -10,3 +10,22 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+const article = document.createElement("article");
+article.classList.add("post");
+document.body.append(article);
+const p = document.createElement("p");
+article.classList.add("post__content");
+p.textContent = "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+article.append(p);
+const footer = document.createElement("footer");
+footer.classList.add("post__footer");
+article.append(footer);
+const span = document.createElement("span");
+span.classList.add("post__username");
+span.textContent = "@username";
+footer.append(span);
+const button = document.createElement("button");
+button.classList.add("post__button");
+button.textContent = "♥ Like";
+footer.append(button);
+button.addEventListener("click",handleLikeButtonClick);
